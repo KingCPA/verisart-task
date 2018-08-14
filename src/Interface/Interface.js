566ea5ba1c64a4ba09123e4a1e72dc6e35f9f833
Aaron King <lbdrking2002@yahoo.co.uk>
2018-08-14 17:03:37 +0100

    refactored components

--- src/Interface/Interface.js
+++ src/Interface/Interface.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
 import './Interface.less';
 
-const Image = require('../Mountains.jpg');
-const divStyle = {
-  width: '100%',
-  height: '1000px',
-  backgroundImage: `url(${Image})`,
-  backgroundSize: 'cover',
-};
-
 class Interface extends Component {
   render() {
     return (
-      <div className='Image' style={divStyle}>
-        <div className='header'>hello</div>
+      <div className='Image'>
+        <div className='header'>
+        <p>Certificate</p>
+        </div>
         <div className='container-1'>
           <div className='box-1'>
-            <h3>Box One</h3>
           </div>
           <div className='box-2'>
             <p>ANTHONY LEPORE</p>
           </div>
+        </div>
+        <div className='box-bottom'>
         </div>
       </div>
     );
